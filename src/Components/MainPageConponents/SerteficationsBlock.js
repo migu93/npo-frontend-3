4e70ac9a03d8eb808cb916ac7f9f8e9012c206a0
Nikita <njkita007@yandex.ru>
2023-07-18 13:26:06 +0300

    Revert "add small fix"
    This reverts commit b277d084

--- src/Components/MainPageConponents/SerteficationsBlock.js
+++ src/Components/MainPageConponents/SerteficationsBlock.js
@@ -6,43 +6,29 @@ import pdf1 from '../../documents/lic1.pdf'
 import pdf2 from '../../documents/gos-taina.pdf'
 import pdf3 from '../../documents/opred-vid-rabot.pdf'
 import pdf4 from '../../documents/thopographic_lic.pdf'
-import {makeStyles} from "@mui/styles";
-
-
-const useStyles = makeStyles((theme) => ({
-    marginY: {
-        textAlign: 'justify',
-        marginTop: theme.spacing(2),
-        marginBottom: theme.spacing(2),
-    },
-}));
 
 export default function Certificates() {
 
-    const classes = useStyles();
+
     return (
         <div>
             <div>
                 <Grid container spacing={3}>
                     <Grid item xs={12} md={6}>
-                        <Typography variant="h4" align="center" gutterBottom>
-                            Сертификаты и Лицензии
+                        <Typography variant="h4" align="left" gutterBottom>
+                            Сертификаты и лицензии
                         </Typography>
-                        <Box>
-                            <Typography style={{marginBottom: '10px', display: 'block'}}>
-                                <span className="first-line" style={{marginLeft: '25px'}}>Для проведения различных видов работ и обеспечения высокого качества услуг, компания имеет в своем распоряжении все необходимые лицензии, сертификаты и разрешительные документы.</span>                            </Typography>
-                            <Typography style={{marginBottom: '10px', display: 'block'}}>
-                                <span className="first-line" style={{marginLeft: '25px'}}>Это подтверждает соответствие наших специалистов требованиям законодательства, а также демонстрирует профессионализм и ответственность компании перед своими клиентами.</span>
-                            </Typography>
-                            <Typography style={{display: 'block'}}>
-                                <span className="first-line" style={{marginLeft: '25px'}}>Для организации работ компания обладает следующим перечнем лицензий и сертификатов:</span>
-                            </Typography>
-                        </Box>
+                        <Typography style={{ textAlign: 'justify' }} mb={3}>
+                            Для проведения различных видов работ и обеспечения высокого качества услуг, компания имеет в своем распоряжении все необходимые лицензии, сертификаты и разрешительные документы. Это подтверждает соответствие наших специалистов требованиям законодательства, а также демонстрирует профессионализм и ответственность компании перед своими клиентами.
+                        </Typography>
+                        <Typography>
+                            Для организации работ компания обладает следующим перечнем лицензий и сертификатов:
+                        </Typography>
                                 <List>
                                     <ListItem disableGutters>
                                         <ListItemText>
                                             <Link href={pdf1} target="_blank" rel="noopener noreferrer">
-                                                <span>лицензией на производство маркшейдерских работ;</span>
+                                                лицензией на производство маркшейдерских работ;
                                             </Link>
                                         </ListItemText>
                                     </ListItem>
